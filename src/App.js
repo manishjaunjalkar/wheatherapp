5867deacf720e73eb2fe6ea7feb9a995fe293277
manishjaunjalkar <manishvjaunjalkar@gmail.com>
2023-05-20 14:55:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () =>{
+      
+     let [cord, setCord] = useState({latitude: "", longitude: ""})
+     let [hemisphere, setHemisphere] = useState("")
+     let [month, setMonth] = useState(new Date().getMonth())
+
+     let monthArr = ["January", "February", "March", "April", "May", "June", "July", "August", "September", 
+      "October", "November", "December"]
+     
+
+     let {latitude, longitude} = cord
+
+       function findLocation(){
+                if(navigator.geolocation){
+                     navigator.geolocation.getCurrentPosition(
+                      (position) => {
+                         console.log(position)
+                         setCord({latitude: position.coords.latitude, longitude: position.coords.longitude})
+                         findHemisphere(position.coords.latitude) 
+                         console.log("Hello World", latitude, longitude, hemisphere)
+                      }
+                     )
+                }
+                       
+         }
+
+         function findHemisphere(latitude){
+           if(latitude>0){
+             setHemisphere("Northern Hemisphere")
+           }else if(latitude<0){
+             setHemisphere("Southern Hemisphere")
+           }else if(latitude===0){
+             setHemisphere("Equator")
+           }
+         }
+         
+      
+
+  return(
+      <div>
+            <h1> LAtitude: {latitude}, Longiitude: {longitude}</h1>
+            <button onClick={findLocation}>Find Location</button>
+            {/* <h1> {latitude>0? "Northen Hemisphere":  latitude<0? "Southern Hemisphere": "Equator"} </h1> */}
+            <h1 > {hemisphere} </h1>
+            <h1> {monthArr[month]} </h1>
+
+      </div>
+  )
 }
 
 export default App;
+
